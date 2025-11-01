44732e25bc639b87b7b9c4577c28a52948901043
operation404 <operation404@gmail.com>
2025-11-01 12:33:18 -0700

    fix options ordering

--- run_container.bat
+++ run_container.bat
@@ -1,3 +1,3 @@
 :: --rm One off container, remove after running
-docker compose -f ".\docker-compose.yml" run Lightbulb-Automation "/bin/bash" --rm
+docker compose -f ".\docker-compose.yml" run --rm Lightbulb-Automation "/bin/bash" 
 cmd /k
